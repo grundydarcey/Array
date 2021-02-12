deb5b5d969313db47df27ee5d25fd6c98a8378ba
Beyonce Knowles <you@example.com>
2021-02-12 14:23:59 -0500

    submit

---
+++ Array.js
@@ -0,0 +1,76 @@
+import Memory from './memory';
+
+//module.exports = Memory;
+
+
+const fakeArray = [1, 2, 3, 4, 5];
+
+class Array {
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;
+    this.ptr = Memory.allocate(this.length);
+  }
+
+  //intialize and push
+
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+
+    Memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = Memory.allocate(size);
+    if (this.ptr === null) {
+      throw new Error('Out of memory');
+    }
+    Memory.copy(this.ptr, oldPtr, this.length);
+    Memory.free(oldPtr);
+    this._capacity = size;
+  }
+
+  //retrieving values
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    return Memory.get(this.ptr + index);
+  }
+
+  //popping values
+  insert(index, value) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+    Memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    Memory.set(this.ptr + index, value);
+    this.length++;
+  }
+
+  //removing values
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    Memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+    this.length--;
+  }
+}
+
+function main() {
+  Array.SIZE_RATIO = 3;
+  let arr = new Array();
+  arr.push(3);
+  console.log(arr);
+}
+
+main();
+Array.SIZE_RATIO = 3;
